a28f2ae3673cfa469962ea37a816f23856e82707
Alejandro <alex.mac.fon@gmail.com>
2023-01-18 19:56:58 -0600

    Tema gruvbox en neovim

--- lua/jmacias/packer.lua
+++ lua/jmacias/packer.lua
@@ -8,9 +8,18 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.1',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
 
+  use({ 
+	  'ellisonleao/gruvbox.nvim', 
+	  as = 'gruvbox',
+	  config = function()
+		  vim.cmd('colorscheme gruvbox')
+	  end
+  })
+
+  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,17 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  gruvbox = {
+    config = { "\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme gruvbox\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/jmacias8075/.local/share/nvim/site/pack/packer/start/gruvbox",
+    url = "https://github.com/ellisonleao/gruvbox.nvim"
+  },
+  ["nvim-treesitter"] = {
+    loaded = true,
+    path = "/home/jmacias8075/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/jmacias8075/.local/share/nvim/site/pack/packer/start/packer.nvim",
@@ -92,6 +103,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: gruvbox
+time([[Config for gruvbox]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\24colorscheme gruvbox\bcmd\bvim\0", "config", "gruvbox")
+time([[Config for gruvbox]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
