53ad1e2e9d04d91d4dda4e038d81cf62f6e3e084
Alejandro <alex.mac.fon@gmail.com>
2023-01-21 15:51:41 -0600

    Indentado de packer.lua

--- lua/jmacias/packer.lua
+++ lua/jmacias/packer.lua
@@ -4,55 +4,55 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
--- Telescope plugin
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Telescope plugin
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
--- Gruvbox Theme
-  use({ 
-	  'ellisonleao/gruvbox.nvim', 
-	  as = 'gruvbox',
-	  config = function()
-		  vim.cmd('colorscheme gruvbox')
-	  end
-  })
+    -- Gruvbox Theme
+    use({ 
+        'ellisonleao/gruvbox.nvim', 
+        as = 'gruvbox',
+        config = function()
+            vim.cmd('colorscheme gruvbox')
+        end
+    })
 
--- Treesitter formatting
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    -- Treesitter formatting
+    use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
--- UndoTree for keeping record of changes in a file
-  use('mbbill/undotree')
+    -- UndoTree for keeping record of changes in a file
+    use('mbbill/undotree')
 
--- LSP for an IDE-like experience
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- LSP for an IDE-like experience
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
 
--- Vimtex for .Tex support
-  use 'lervag/vimtex'
+    -- Vimtex for Tex support
+    use 'lervag/vimtex'
 
 end)
