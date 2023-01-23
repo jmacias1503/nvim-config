596dccae6c71a5c940610476ed1ee8521e966c2c
Alejandro <alex.mac.fon@gmail.com>
2023-01-23 17:22:13 -0600

    Alch no me acuerdo q iba en este commit

--- lua/jmacias/remap.lua
+++ lua/jmacias/remap.lua
@@ -1,19 +1,19 @@
 vim.g.mapleader = " "
 
--- Abrir explorador
+-- Open neovim file explorer
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 
--- Mover region de codigo
+-- Move code region
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Copiar codigo al portapapeles
+-- Copy text to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- Reemplazar palabra dentro del archivo
+-- Replace whole word in the entire file
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
---Creacion y cambio de tabs
+-- Creation and tab movement
 vim.keymap.set("n", "<leader>tn", vim.cmd.tabNext)
 vim.keymap.set("n", "<leader>nt", vim.cmd.tabnew)
