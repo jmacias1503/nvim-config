274976ea68455db4aad45819eb5bcc63d77730f4
Alejandro <alex.mac.fon@gmail.com>
2023-02-26 19:29:17 -0600

    Control de highlighting de latex a vimtex en vez de treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -26,6 +26,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
+    disable = {"latex"},
 
     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
@@ -38,6 +39,6 @@ require'nvim-treesitter.configs'.setup {
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = {"latex"},
   },
 }
