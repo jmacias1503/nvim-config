1fbc74cd93a4e8f4d1e9dec971143516190b3456
Alejandro <alex.mac.fon@gmail.com>
2023-03-14 19:17:08 -0600

    Adicion del plugin trouble para visualizacion de problemas en el codigo

--- lua/jmacias/packer.lua
+++ lua/jmacias/packer.lua
@@ -180,4 +180,19 @@ use {
 -- Vimtex
 use 'lervag/vimtex'
 
+-- Trouble viewing
+use {
+  "folke/trouble.nvim",
+  requires = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
+
+
 end)
