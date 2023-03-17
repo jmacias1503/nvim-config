8ff977eb45fde3293173fa812b22a48e2c16039e
Alejandro <alex.mac.fon@gmail.com>
2023-03-16 18:32:00 -0600

    Removido el vimwiki del after, cambiado de locacion a packer

--- lua/jmacias/packer.lua
+++ lua/jmacias/packer.lua
@@ -194,7 +194,14 @@ use {
 }
 
 --Vimwiki
-use 'vimwiki/vimwiki'
+use {
+    'vimwiki/vimwiki',
+    config = function ()
+        vim.g.vimwiki_list = {{path = '~/Docs/Mywiki', syntax = 'markdown', ext = '.md'}}
+        vim.g.vimwiki_global_ext = 0
+        vim.g.vimwiki_ext2syntax = {['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown'}
+    end
+}
 
 
 
