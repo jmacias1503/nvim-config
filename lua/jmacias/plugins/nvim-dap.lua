06832d5c28cbf5c25e274e0c71a72d3f5a36428d
Alejandro <alex.mac.fon@gmail.com>
2023-04-20 20:34:46 -0600

    dap-ui como dependencia

--- lua/jmacias/plugins/nvim-dap.lua
+++ lua/jmacias/plugins/nvim-dap.lua
@@ -1,4 +1,8 @@
 return {
+
+   dependencies = {
+      'rcarriga/nvim-dap-ui',
+   },
    'mfussenegger/nvim-dap',
    config = function ()
       vim.keymap.set("n", "<leader>bp", vim.cmd.DapToggleBreakpoint)
