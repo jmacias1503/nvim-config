d23d9e65596e9acd34f2c1ae6a0a10512d83b3c5
Alejandro <alex.mac.fon@gmail.com>
2023-05-07 18:27:55 -0600

    Docs folder changed to documents

--- lua/jmacias/plugins/orgmode.lua
+++ lua/jmacias/plugins/orgmode.lua
@@ -6,9 +6,9 @@ return {
 	config = function()
 		require('orgmode').setup_ts_grammar{}
 		require('orgmode').setup{
-			org_agenda_files = {'/home/jmacias8075/Docs/Second Brain/org/*'},
-			org_default_notes_file = '~/Docs/Second Brain/org/default.org',
-         org_archive_location = '/home/jmacias8075/Docs/Second Brain/org/archive.org',
+			org_agenda_files = {'/home/jmacias8075/Documents/Second Brain/org/*'},
+			org_default_notes_file = '~/Documents/Second Brain/org/default.org',
+         org_archive_location = '/home/jmacias8075/Documents/Second Brain/org/archive.org',
 		}
 	end,
    ft = "org",
--- lua/jmacias/plugins/vimwiki.lua
+++ lua/jmacias/plugins/vimwiki.lua
@@ -1,7 +1,7 @@
 return {
    'vimwiki/vimwiki',
    init = function ()
-      vim.g.vimwiki_list = {{path = '~/Docs/Second Brain', syntax = 'markdown', ext = '.md'}}
+      vim.g.vimwiki_list = {{path = '~/Documents/Second Brain', syntax = 'markdown', ext = '.md'}}
       vim.g.vimwiki_global_ext = 0
    end,
    ft = "markdown",
