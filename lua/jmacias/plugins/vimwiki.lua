f145767ceae54acac95cb09e84e0a4cb8175f77d
Alejandro <alex.mac.fon@gmail.com>
2023-06-25 21:05:39 -0600

    adicion de path relativo para el daiario vimwiki

--- lua/jmacias/plugins/vimwiki.lua
+++ lua/jmacias/plugins/vimwiki.lua
@@ -3,6 +3,8 @@ return {
    init = function ()
       vim.g.vimwiki_list = {{path = '~/Documents/Second Brain/Notas', syntax = 'markdown', ext = '.md'}}
       vim.g.vimwiki_global_ext = 0
+      vim.g.vimwiki_listsyms = ' ○◐●x'
+      vim.g.vimwiki_diary_rel_path = ''
    end,
    ft = "markdown",
    keys = {{"<leader>ww"}, {"<leader>wi"}},
